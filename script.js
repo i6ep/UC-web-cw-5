5d17d57c0aa3ae6bbdbfdaffb6ca3695b2cf8fac
i6ep <142681557+i6ep@users.noreply.github.com>
2023-09-04 20:02:05 +0300

    70

---
+++ script.js
@@ -0,0 +1,9 @@
+
+alert('مرحباً بك في موقعي!انا اسمي فجر و سوف احسب درجتك')
+let grade = parseInt(prompt('ادخل درجتك'))
+
+console.log(grade)
+
+    if ( grade >=90 && grade <=100 ) {
+        console.log("لقد حصلت على امتياز");
+    }
